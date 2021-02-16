937aac177b3dfa7af82b1ebc0dc4d2b21bff1889
student <student@itstep1.dp.ua>
2021-02-16 18:18:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationanimation/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplicationanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setBackgroundResource(R.drawable.wateranimation);
+        final AnimationDrawable myAnim = (AnimationDrawable) img.getBackground();
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAnim.start();
+            }
+        });
+    }
+}
